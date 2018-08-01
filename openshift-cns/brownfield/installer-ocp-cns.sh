aeb249966ac1e456fd703ce17910fc8ff17ebd84
Husnain Bustam <habbdt@gmail.com>
2018-08-01 09:05:19 -0400

    file added

--- openshift-cns/brownfield/installer-ocp-cns.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#ansible-playbook -i ocp-brownfield.hosts  /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
-ansible-playbook -i ocp-brownfield.hosts  /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
-
